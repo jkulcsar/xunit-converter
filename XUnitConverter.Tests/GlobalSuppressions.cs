0b9008cb7613bb4a9539e9afea901ca547bd28bf
Thijs Brobbel <thijsbrobbel@gmail.com>
2019-10-09 21:51:50 +0200

    Suppress AsyncFixer01 diagnostics,
    since they don't add any value in this case.

---
+++ XUnitConverter.Tests/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "AsyncUsage",
+    "AsyncFixer01:Unnecessary async/await usage",
+    Justification = "All tests have been written with a certain pattern where this diagnostic fires, and makes it less valuable (right now).")]
