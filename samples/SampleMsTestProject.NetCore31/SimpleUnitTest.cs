83d9a003f42de8c792f540fec11ec6969579a889
Thijs Brobbel <thijsbrobbel@gmail.com>
2020-05-02 14:46:50 +0200

    Add simple sample for netcore3.1

---
+++ samples/SampleMsTestProject.NetCore31/SimpleUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleMsTestProject.NetCore31
+{
+    [TestClass]
+    public class SimpleUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
