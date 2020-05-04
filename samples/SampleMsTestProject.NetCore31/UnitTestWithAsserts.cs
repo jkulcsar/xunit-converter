4840bd7a8806c1a2c6cf81cf9a0e7583588b0a16
Thijs Brobbel <thijsbrobbel@gmail.com>
2020-05-04 11:04:59 +0200

    Add some simple asserts to sample

---
+++ samples/SampleMsTestProject.NetCore31/UnitTestWithAsserts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleMsTestProject.NetCore31
+{
+    [TestClass]
+    public class UnitTestWithAsserts
+    {
+        [TestMethod]
+        public void BooleanAssert()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void IntegerAssert()
+        {
+            var expected = 1;
+            Assert.AreEqual(expected, 1);
+        }
+    }
+}
